4a855eb3396a967818c93bc6e4fd62f086e1f39a
DonArtkins <info.sycx.ke@gmail.com>
2025-03-01 15:10:50 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red-600">Home Page</h1>
+    <RouterLink to="/projects">Go To Projects</RouterLink>
+  </div>
+</template>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-yellow-600">Projects Page</h1>
+    <RouterLink to="/">Go To Home</RouterLink>
+  </div>
+</template>
